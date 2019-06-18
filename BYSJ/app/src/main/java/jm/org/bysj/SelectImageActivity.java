d3b7edc7d0945cc503aa1189d5b697d1407b30cf
FatWhite <3338504@qq.com>
2019-06-18 18:51:18 +0800

    a

--- BYSJ/app/src/main/java/jm/org/bysj/SelectImageActivity.java
+++ BYSJ/app/src/main/java/jm/org/bysj/SelectImageActivity.java
@@ -2,7 +2,6 @@ package jm.org.bysj;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
@@ -18,10 +17,7 @@ import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.google.gson.Gson;
-import com.google.gson.JsonArray;
-
-import org.json.JSONException;
-import org.json.JSONObject;
+import com.zhy.base.fileprovider.FileProvider7;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -41,12 +37,16 @@ import jm.org.bysj.view.InputDialog;
  */
 public class SelectImageActivity extends Activity implements View.OnClickListener,InputDialog.InputDialogListener{
 
-    public static final int RC_CHOOSE_PHOTO = 2;
+    public static final int TAKE_PHOTO = 1;
+    public static final int CHOOSE_PHOTO = 2;
     public static final String TAG=SelectImageActivity.class.getSimpleName();
 
+    private String mTempPhotoPath;
+    private Uri imageUri;
     private RelativeLayout relativeLayout;
     private ImageView imageView;
     private Button btnSelect;
+    private Button btnTakePhoto;
     private Button btnAdd;
     private Button btnSave;
     private InputDialog inputDialog;
@@ -66,6 +66,7 @@ public class SelectImageActivity extends Activity implements View.OnClickListene
         btnSelect=findViewById(R.id.btn_select);
         btnAdd=findViewById(R.id.btn_add_tag);
         btnSave=findViewById(R.id.btn_save_tag);
+        btnTakePhoto=findViewById(R.id.btn_take_photo);
         tvTitle=findViewById(R.id.tv_title);
         tvTitle.setText("选择图片");
         ivBack=findViewById(R.id.iv_back);
@@ -74,20 +75,39 @@ public class SelectImageActivity extends Activity implements View.OnClickListene
         btnSelect.setOnClickListener(this);
         btnAdd.setOnClickListener(this);
         btnSave.setOnClickListener(this);
+        btnTakePhoto.setOnClickListener(this);
         inputDialog.setInputDialogListener(this);
     }
 
     private void selectPhoto() {
         Intent intentToPickPic = new Intent(Intent.ACTION_PICK, null);
         intentToPickPic.setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*");
-        startActivityForResult(intentToPickPic, RC_CHOOSE_PHOTO);
+        startActivityForResult(intentToPickPic, CHOOSE_PHOTO);
+    }
+
+
+    private void takePhoto() {
+        Intent intentToTakePhoto = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        File fileDir = new File(ImageUtils.getSDPath());
+        if (!fileDir.exists()) {
+            fileDir.mkdirs();
+        }
+
+        File photoFile = new File(fileDir, "takephoto.png");
+        mTempPhotoPath = photoFile.getAbsolutePath();
+        imageUri = FileProvider7.getUriForFile(this, photoFile);
+        intentToTakePhoto.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+        startActivityForResult(intentToTakePhoto, TAKE_PHOTO);
     }
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
         switch (requestCode) {
-            case RC_CHOOSE_PHOTO:
+            case TAKE_PHOTO:
+                Glide.with(this).load(mTempPhotoPath).into(imageView);
+                break;
+            case CHOOSE_PHOTO:
                 if (data!=null){
                     Uri uri = data.getData();
                     String filePath = FileUtil.getFilePathByUri(this, uri);
@@ -102,6 +122,9 @@ public class SelectImageActivity extends Activity implements View.OnClickListene
     @Override
     public void onClick(View v) {
         switch (v.getId()){
+            case R.id.btn_take_photo:
+                takePhoto();
+                break;
             case R.id.btn_select:
                 selectPhoto();
                 break;
